49804c5ec67dcee1a4873f917cf60c84751f4923
Joao Vitor <joaovsantos.dev@gmail.com>
2025-02-07 14:18:13 -0300

    adicionando interação com radio button

--- projeto/script.js
+++ projeto/script.js
@@ -6,22 +6,22 @@ radioInput.forEach((radio) => {
     radioInput.forEach((item) => {
       if (item.value === event.target.value) {
         const parentRadio = item.parentElement;
-        item.classList.add('active');
-        item.classList.add('hidden-radio');
+        item.classList.add("active");
+        item.classList.add("hidden-radio");
         parentRadio.style.backgroundColor = "rgba(12, 125, 105, 0.3)";
         parentRadio.style.borderColor = "rgba(12, 125, 105, 1)";
       } else {
+        item.classList.remove("active");
+        item.classList.remove("hidden-radio");
         const parentRadio = item.parentElement;
-        item.classList.remove('active')
-        item.classList.remove('hidden-radio')
+        item.classList.remove("active");
+        item.classList.remove("hidden-radio");
         parentRadio.style.backgroundColor = "#fff";
         parentRadio.style.borderColor = " rgba(43, 66, 70, 0.5)";
       }
     });
   });
 });
-
-
 
 // ----------------------------------------------------------------------
 
@@ -32,6 +32,8 @@ const lastNameInput = document.getElementById("last-name");
 const emailInput = document.getElementById("email");
 const textAreaInput = document.getElementById("text-area");
 
+const radio1 = document.getElementById("radio1");
+const radio2 = document.getElementById("radio2");
 
 // Evento submit do botão principal para executar o método geral
 form.addEventListener("submit", (event) => {
@@ -48,12 +50,14 @@ function checkInputGeneral() {
   checkInputTextArea();
   checkRadioButtons();
 }
+checkedInputClick();
+
 
 // Verificações de inputs
 
 function checkInputName() {
   const firstNameValue = firstNameInput.value;
-  
+
   if (firstNameValue === "") {
     errorMsg(firstNameInput);
   } else {
@@ -64,7 +68,7 @@ function checkInputName() {
 
 function checkInputLastName() {
   const lastNameValue = lastNameInput.value;
-  
+
   if (lastNameValue === "") {
     errorMsg(lastNameInput);
   } else {
@@ -73,10 +77,9 @@ function checkInputLastName() {
   }
 }
 
-
 function checkInputEmail() {
   const emailValue = emailInput.value;
-  
+
   if (emailValue === "") {
     errorMsg(emailInput);
   } else {
@@ -85,22 +88,25 @@ function checkInputEmail() {
   }
 }
 
-
 function checkRadioButtons() {
-  const radio1 = document.getElementById("radio1");
-  const radio2 = document.getElementById("radio2");
-  
   if (!radio1.checked && !radio2.checked) {
     msgErroRadio();
-  } else {
-    const parentRadio = document.querySelector('.form-query')
-    parentRadio.classList.remove('erro');
   }
+}
+function checkedInputClick() {
+  radioInput.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      if (item.value === event.target.value) {
+        const parentInput = item.parentElement.parentElement;
+        parentInput.classList.remove("erro");
+      }
+    });
+  });
 }
 
 function checkInputTextArea() {
   const textAreaValue = textAreaInput.value;
-  
+
   if (textAreaValue === "") {
     errorMsg(textAreaInput);
   } else {
@@ -108,8 +114,6 @@ function checkInputTextArea() {
     parentInput.classList.remove("erro");
   }
 }
-
-
 
 // Métodos de erro
 
@@ -120,5 +124,5 @@ function errorMsg(input) {
 
 function msgErroRadio() {
   const parentRadio = document.querySelector(".form-query");
-  parentRadio.classList.add('erro')
+  parentRadio.classList.add("erro");
 }
