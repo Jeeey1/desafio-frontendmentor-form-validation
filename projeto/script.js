e696329ae6c216bf1382afd495542ae938273d39
Joao Vitor <joaovsantos.dev@gmail.com>
2025-02-10 14:43:47 -0300

    organização do código javascript

--- projeto/script.js
+++ projeto/script.js
@@ -1,6 +1,52 @@
+// Declaração das variáveis necessárias para início da lógica
+const form = document.getElementById("form");
+const usernameForm = document.querySelector(".form-username");
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const emailInput = document.getElementById("email");
+const textAreaInput = document.getElementById("text-area");
+const checkboxInput = document.getElementById('term');
+const radio1 = document.getElementById("radio1");
+const radio2 = document.getElementById("radio2");
 const radioInput = document.querySelectorAll("input[type=radio]");
 const imgRadio = document.querySelector(".radio-checked");
 
+
+// Evento submit do botão principal para executar o método geral
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  checkForm();
+});
+
+
+// Método geral contendo todos os outros métodos de input
+function checkForm(){
+  checkInputGeneral();
+
+  const formItems = form.querySelectorAll('div[class^="form"]');
+  console.log(formItems);
+
+  const isValid = [...formItems].every((item) => {
+    return !item.classList.contains('erro');
+  })
+  console.log(isValid)
+
+  if(isValid){
+    messageSubmit();
+  }
+}
+
+
+// Ativa mensagem de enviado dados com sucesso caso todos os inputs estejam corretos
+function messageSubmit(){
+  const popUpMessage = document.querySelector(".success-msg");
+  popUpMessage.classList.add('success-active');
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+// Loop para alterar cor dos radio-button ao clicar
 radioInput.forEach((radio) => {
   radio.addEventListener("change", (event) => {
     radioInput.forEach((item) => {
@@ -21,66 +67,8 @@ radioInput.forEach((radio) => {
   });
 });
 
-// ----------------------------------------------------------------------
 
-const form = document.getElementById("form");
-const usernameForm = document.querySelector(".form-username");
-const firstNameInput = document.getElementById("first-name");
-const lastNameInput = document.getElementById("last-name");
-const emailInput = document.getElementById("email");
-const textAreaInput = document.getElementById("text-area");
-const checkboxInput = document.getElementById('term');
-
-const radio1 = document.getElementById("radio1");
-const radio2 = document.getElementById("radio2");
-
-// Evento submit do botão principal para executar o método geral
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  checkForm();
-});
-
-// Método geral contendo todos os outros métodos de input
-function checkForm(){
-  checkInputGeneral();
-
-  const formItems = form.querySelectorAll('div[class^="form"]');
-  console.log(formItems);
-
-  const isValid = [...formItems].every((item) => {
-    return !item.classList.contains('erro');
-  })
-  console.log(isValid)
-
-  if(isValid){
-    messageSubmit();
-  }
-}
-
-function messageSubmit(){
-  const popUpMessage = document.querySelector(".success-msg");
-  popUpMessage.classList.add('success-active');
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-
-function checkInputGeneral() {
-  checkInputName();
-  checkInputLastName();
-  checkInputEmail();
-  checkInputTextArea();
-  checkRadioButtons();
-  checkInputCheckbox();
-}
-
-
-// Bolinha verde do radio input
-checkedInputClick();
-
-
-// Verificações de inputs
-
+// Verificações do Input First Name
 function checkInputName() {
   const firstNameValue = firstNameInput.value;
 
@@ -91,9 +79,11 @@ function checkInputName() {
   }
 }
 
+
+// Verificações do Input Last Name
 function checkInputLastName() {
   const lastNameValue = lastNameInput.value;
-
+  
   if (lastNameValue === "") {
     errorMsgName(lastNameInput);
   } else {
@@ -101,16 +91,17 @@ function checkInputLastName() {
   }
 }
 
+
+// Verificações do Input Email
 function checkInputEmail() {
   const emailValue = emailInput.value;
-
+  
   if (emailValue === '' || !isEmailValid(emailValue)){
     errorMsg(emailInput);
   } else {
     removeErrorMsg(emailInput);
   }
 }
-
 // Valida o email digitado
 function isEmailValid(email) {
   const emailRegex = new RegExp(
@@ -122,25 +113,19 @@ function isEmailValid(email) {
   return false;
 }
 
+
+// Verificações do Input Radio Button
 function checkRadioButtons() {
   if (!radio1.checked && !radio2.checked) {
     errorMsgRadio();
   }
 }
-function checkedInputClick() {
-  radioInput.forEach((item) => {
-    item.addEventListener("click", (event) => {
-      if (item.value === event.target.value) {
-        const parentInput = item.parentElement.parentElement;
-        parentInput.classList.remove("erro");
-      }
-    });
-  });
-}
 
+
+// Verificações do Input TextArea
 function checkInputTextArea() {
   const textAreaValue = textAreaInput.value;
-
+  
   if (textAreaValue === "") {
     errorMsg(textAreaInput);
   } else {
@@ -148,6 +133,8 @@ function checkInputTextArea() {
   }
 }
 
+
+// Verificações do Input Checkbox
 function checkInputCheckbox(){
   const checkboxValue = checkboxInput.checked;
   if (!checkboxValue){
@@ -157,6 +144,8 @@ function checkInputCheckbox(){
   }
 }
 
+
+// Adiciona imagem de checkbox check e oculta o checkbox button padrão do navegador
 checkboxInput.addEventListener('click', (event => {
   const parentElement = checkboxInput.parentElement;
   if(event.target.checked){
@@ -167,22 +156,37 @@ checkboxInput.addEventListener('click', (event => {
     checkboxInput.style.visibility = 'visible'
   }
 }))
-// Métodos de erro
 
+
+// função que contém todas as funções de check input
+function checkInputGeneral() {
+  checkInputName();
+  checkInputLastName();
+  checkInputEmail();
+  checkInputTextArea();
+  checkRadioButtons();
+  checkInputCheckbox();
+}
+
+
+// Funções de erro
 function errorMsg(input) {
   const parentInput = input.parentElement;
   parentInput.classList.add("erro");
 }
+
 
 function removeErrorMsg(input){
   const parentInput = input.parentElement;
   parentInput.classList.remove("erro");
 }
 
+
 function errorMsgName(input) {
   const parentInput = input.parentElement.parentElement;
   parentInput.classList.add("erro");
 }
+
 
 function removeErrorMsgName(input){
   const parentInput = input.parentElement.parentElement;
